1a1970baf8c13e22ada1e5f975f7a4e97cc7f4e7
clarknoah <noahbc08@gmail.com>
2017-09-29 17:36:03 +0400

    did some practice with inheritence

--- app.ts
+++ app.ts
@@ -1,14 +1,55 @@
-import { NgModule, Component, EventEmitter
+import {
+  NgModule, Component, EventEmitter
 } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+class Life{
+  species: string;
+
+  constructor(name: string){
+    this.species = name;
+  }
+
+  exist(){
+    console.log(`This species of life is called: ${this.species}`);
+  }
+
+}
+
+class Animal extends Life{
+
+  constructor(name: string){
+    super(name);
+  }
+  exist(){
+    console.log(`I eat Plants`);
+    super.exist();
+  }
+
+
+}
+
+class Plant extends Life{
+  c02_converted: number;
+  constructor(name: string){
+    super(name);
+  }
+
+  convertC02():void{
+    this.c02_converted += 1;
+    console.log(`Wow, you've converted ${this.c02_converted} units of C02`);
+  }
+
+
+}
+
 @Component({
-  selector:'list-of-stuff',
-  inputs:['listOfStuff'],
-  outputs:['selectedItemEvent'],
-  template:`
+  selector: 'list-of-stuff',
+  inputs: ['listOfStuff'],
+  outputs: ['selectedItemEvent'],
+  template: `
   <ul>
     <li *ngFor="let name of listOfStuff">
       <button (click)="clickedListItem(name)">{{name}}</button>
@@ -19,18 +60,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 })
 
 
-class ListOfStuffComponent{
+class ListOfStuffComponent {
 
   selectedItem: string;
   selectedItemEvent: EventEmitter<string>;
 
-  constructor(){
+  constructor() {
     this.selectedItemEvent = new EventEmitter();
   }
-  selectListItem($event): void{
+  selectListItem($event): void {
     this.selectedItem = $event.target;
   }
-  clickedListItem(name:string): void{
+  clickedListItem(name: string): void {
     this.selectedItem = name;
     this.selectedItemEvent.emit(name);
     console.log(name);
@@ -41,37 +82,38 @@ class ListOfStuffComponent{
 
 
 @Component({
-  selector:'input-output',
-  template:`
-    <h1>Selected Name: {{title}}</h1>
+  selector: 'input-output',
+  template: `
+    <h1>Selected Name: {{selectedListItem}}</h1>
     <list-of-stuff
     [listOfStuff]="items"
     (selectedItemEvent)="selectionHasChanged($event)"
     >
     </list-of-stuff>
-
   `
-
 })
 
-class InputOutputApp{
+class InputOutputApp {
 
   items: string[];
-  title: string;
+  selectedListItem: string;
+  bob: Animal;
 
-  constructor(){
+  constructor() {
+    this.bob = new Animal('Horse');
     this.items = [
       'Noah',
       'Elizabeth',
       'Leah'
     ];
-    this.title = "Testing Input Output Baby!";
+    this.selectedListItem = "Testing Input Output Baby!";
   }
 
-    selectionHasChanged(name:string): void{
-     console.log(`It worked baby: ${name}`);
-     this.title = name;
-    }
+  selectionHasChanged(name: string): void {
+    console.log(`It worked baby: ${name}`);
+    this.bob.exist();
+    this.selectedListItem = name;
+  }
 
 }
 
@@ -79,8 +121,8 @@ class InputOutputApp{
   declarations: [
     InputOutputApp,
     ListOfStuffComponent],
-    imports:[BrowserModule],
-    bootstrap:[InputOutputApp]
+  imports: [BrowserModule],
+  bootstrap: [InputOutputApp]
 })
 
 class InputOutputAppModule { } platformBrowserDynamic().bootstrapModule(InputOutputAppModule);
